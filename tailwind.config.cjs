5e4d54c7626fe4a5eb318b4db8fbfcf7e9902db0
essofyany <soufyani.bilal@gmail.com>
2022-05-07 00:29:05 +0000

    setup prisma

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,26 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [require('@tailwindcss/forms')]
+	plugins: [
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/forms'),
+		// https://gist.github.com/samselikoff/b3c5126ee4f4e69e60b0af0aa5bfb2e7
+		plugin(function ({ addVariant, e, postcss }) {
+			addVariant('firefox', ({ container, separator }) => {
+				const isFirefoxRule = postcss.atRule({
+					name: '-moz-document',
+					params: 'url-prefix()'
+				});
+				isFirefoxRule.append(container.nodes);
+				container.append(isFirefoxRule);
+				isFirefoxRule.walkRules((rule) => {
+					rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`;
+				});
+			});
+		})
+	]
 };
